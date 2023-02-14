ca3c8ba2668086c802b62acccd51db95e197b812
Nathan Seva <thykof@protonmail.ch>
2023-02-14 16:01:57 +0100

    run ansible playbook in golang test file

--- cmd/node_manager_tester/main.go
+++ cmd/node_manager_tester/main.go
@@ -1,76 +1,34 @@
 package main
 
 import (
-	"log"
-	"runtime"
-	"time"
-
-	"github.com/massalabs/thyra-node-manager-plugin/pkg/node_manager"
+	ansibler "github.com/febrianrendak/go-ansible"
 )
 
 func installMassaNode() {
-	os := runtime.GOOS
-	arch := runtime.GOARCH
-
-	link, err := node_manager.GetMassaNodeLink(os, arch)
-	if err != nil {
-		log.Fatalln(err)
+	ansiblePlaybookConnectionOptions := &ansibler.AnsiblePlaybookConnectionOptions{
+		Connection: "ssh",
+		PrivateKey: "./automation/ubuntu/idkey", // ansible_ssh_private_key_file
+		User:       "massa-ssh",                 // ansible_user
 	}
 
-	log.Println("Link: ", link)
-	node_manager.DownloadAndUnarchiveNode(link)
-	log.Println("Massa Node installed")
+	ansiblePlaybookOptions := &ansibler.AnsiblePlaybookOptions{
+		Inventory: "192.168.1.50,",
+	}
+
+	playbook := &ansibler.AnsiblePlaybookCmd{
+		Playbook:          "./automation/playbook.yml",
+		ConnectionOptions: ansiblePlaybookConnectionOptions,
+		Options:           ansiblePlaybookOptions,
+		ExecPrefix:        "Go-ansible ",
+	}
+
+	err := playbook.Run()
+	if err != nil {
+		panic(err)
+	}
+
 }
 
 func main() {
 	installMassaNode()
-	nodeRunner := node_manager.NodeRunner{}
-	err := nodeRunner.StartNode(
-		node_manager.Node{
-			Id:   1,
-			Name: "Test",
-			Ip:   "localhost",
-		},
-	)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	for i := 10; i > 0; i-- {
-		log.Printf("Shutting down node in %d seconds...", i)
-		time.Sleep(1 * time.Second)
-	}
-
-	err = nodeRunner.StopNode()
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	nodes, err := node_manager.GetNodes()
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	log.Println("Nodes: ", nodes)
-
-	node := node_manager.Node{
-		Id:   1,
-		Name: "Test",
-		Ip:   "localhost",
-	}
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	err = node_manager.AddNode(node)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	nodes, err = node_manager.GetNodes()
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	log.Println("AFTER ADD Nodes: ", nodes)
 }
