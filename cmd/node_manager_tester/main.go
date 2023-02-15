6db47110fb249891e40ec87f9257ee0495827156
Nathan Seva <thykof@protonmail.ch>
2023-02-15 11:29:39 +0100

    POC: start and stop with ssh

--- cmd/node_manager_tester/main.go
+++ cmd/node_manager_tester/main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	ansibler "github.com/febrianrendak/go-ansible"
+	"github.com/sfreiberg/simplessh"
 )
+
+const host = "192.168.1.50"
+const keyFile = "./automation/ubuntu/idkey"
+const username = "massa-ssh"
 
 func installMassaNode() {
 	ansiblePlaybookConnectionOptions := &ansibler.AnsiblePlaybookConnectionOptions{
 		Connection: "ssh",
-		PrivateKey: "./automation/ubuntu/idkey", // ansible_ssh_private_key_file
-		User:       "massa-ssh",                 // ansible_user
+		PrivateKey: keyFile,  // ansible_ssh_private_key_file
+		User:       username, // ansible_user
 	}
 
 	ansiblePlaybookOptions := &ansibler.AnsiblePlaybookOptions{
-		Inventory: "192.168.1.50,",
+		Inventory: host + ",",
 	}
 
 	playbook := &ansibler.AnsiblePlaybookCmd{
@@ -29,6 +37,42 @@ func installMassaNode() {
 
 }
 
+func getSSHClient() *simplessh.Client {
+	client, err := simplessh.ConnectWithKeyFile(host, username, keyFile)
+	if err != nil {
+		panic(err)
+	}
+
+	return client
+}
+
+func startNode() {
+	client := getSSHClient()
+	defer client.Close()
+
+	output, err := client.Exec("sudo docker compose up -d")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("docker-compose up: %s\n", output)
+}
+
+func stopNode() {
+	client := getSSHClient()
+	defer client.Close()
+
+	output, err := client.Exec("sudo docker compose stop")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("docker-compose stop: %s\n", output)
+}
+
 func main() {
 	installMassaNode()
+	startNode()
+	time.Sleep(5 * time.Second)
+	stopNode()
 }
