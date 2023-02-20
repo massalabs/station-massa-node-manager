c040a5589b7399b986cd2b2b3185efc0e5455233
Pierre Seznec <ps@massa.net>
2023-02-20 14:06:59 +0100

    fix install bash script indent

--- pkg/node_manager/node_installer.go
+++ pkg/node_manager/node_installer.go
@@ -49,31 +49,31 @@ func Install(node Node) {
 	}
 
 	dockerInstallScript := fmt.Sprintf(`
-	sudo apt-get update
-	sudo apt-get install -y curl jq
-	curl -fsSL https://get.docker.com -o get-docker.sh
-	sudo sh ./get-docker.sh
-	cat << '	EOF' > .env
-	DISCORD_ID=%s
-	WALLETPWD=%s
-	EOF
-	cat << '	EOF' > docker-container-logrotate
-	/var/lib/docker/containers/*/*.log {
-		rotate 0
-		hourly
-		notifempty
-		nocompress
-		size 50M
-		missingok
-		copytruncate
-		nodateext
-		maxage 1
+sudo apt-get update
+sudo apt-get install -y curl jq
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh ./get-docker.sh
+cat << 'EOF' > .env
+DISCORD_ID=%s
+WALLETPWD=%s
+EOF
+cat << 'EOF' > docker-container-logrotate
+/var/lib/docker/containers/*/*.log {
+	rotate 0
+	hourly
+	notifempty
+	nocompress
+	size 50M
+	missingok
+	copytruncate
+	nodateext
+	maxage 1
 	}
-	EOF
-	sudo mv docker-container-logrotate /etc/logrotate.d/docker-container-logrotate
-	sudo docker compose up -d --pull always --remove-orphans
-	echo "Node installation completed"
-	`, node.DiscordId, node.WalletPassword)
+EOF
+sudo mv docker-container-logrotate /etc/logrotate.d/docker-container-logrotate
+sudo docker compose up -d --pull always --remove-orphans
+echo "Node installation completed"
+`, node.DiscordId, node.WalletPassword)
 
 	output, err := node.runCommandSSH(dockerInstallScript)
 	if err != nil || !strings.Contains(string(output), "Node installation completed") {
