93a719e2f65e12f6a2a0b2df88baf3da2dce6f15
Pierre Seznec <ps@massa.net>
2023-02-20 11:32:05 +0100

    add jq

--- pkg/node_manager/node_installer.go
+++ pkg/node_manager/node_installer.go
@@ -48,7 +48,7 @@ func Install(node Node) {
 
 	dockerInstallScript := fmt.Sprintf(`
 	sudo apt-get update \
-	sudo apt-get install -y curl \
+	sudo apt-get install -y curl jq \
 	curl -fsSL https://get.docker.com -o get-docker.sh
 	sudo sh ./get-docker.sh
 	cat << 'EOF' > .env
